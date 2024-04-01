f7409def4aa44a0bc251856fa64282ddbe6ab3e3
nitishindia <156898742+nitishindia@users.noreply.github.com>
2024-04-01 22:13:53 +0530

    Update A.java

--- JAVA/src/Array/A.java
+++ JAVA/src/Array/A.java
@@ -14,10 +14,6 @@ public class A {
 		System.out.println(a[1]);
 		System.out.println(a[2]);
 		System.out.println(a[3]);
-		System.out.println(a[4]);
-		
-		
-		
+		System.out.println(a[4]);		
 	}
-
 }
